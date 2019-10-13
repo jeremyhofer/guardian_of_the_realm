be61c41f092966a8c26898d4ba3721241d86ee4e
_Zorbaz <z_e32@ymail.com>
2019-10-13 15:46:38 -0600

    18.10.19 amendment - it's friday!

--- commands/general.js
+++ commands/general.js
@@ -3,8 +3,17 @@ module.exports = {
     // Show help text. optional specific command
   },
   bal (args, client, msg) {
+
+  let player_data = null;
+
+  // Get player data - use defaults if new
+  player_data = client.getPlayer.get(msg.author.id);
+    if (!player_data) {
+      player_data = {...client.defaultPlayerData};
+      player_data.user = msg.author.id;
+      client.setPlayer.run(player_data);}
     // Lists the players money, men, and ships with the unique faction name for each.
-    msg.reply("Our finest artificers are working night and day; we apologize for not being able to find your records.");
+    msg.reply(`Your account: ${player_data.money} :moneybag: ${player_data.men} :MenAtArms: ${player_data.ships} :Warship:`);
     return 0;
   }
 };
