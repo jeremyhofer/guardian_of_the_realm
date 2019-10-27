f73ce1d9a6b061c3757e42d294b752684e288213
Jeremy Hofer <fuego0607@gmail.com>
2019-10-26 20:56:18 -0400

    ADD database setup and command definition from bot.js to database.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
-const SQLite = require("better-sqlite3");
-const sql = new SQLite('./data/gotr_bot.sqlite');
+const db = require('./database.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
 const PREFIX = '.';
@@ -21,110 +20,14 @@ const command_dispatch = {
 };
 
 client.on("ready", () => {
-  // Check if the table "player_data" exists.
-  const player_table = sql.prepare(`
-    SELECT count(*) FROM sqlite_master
-    WHERE type='table' AND name = 'player_data';
-  `).get();
-  if (!player_table['count(*)']) {
-    // If the table isn't there, create it and setup the database correctly.
-    sql.prepare(`
-      CREATE TABLE player_data (
-        user TEXT PRIMARY KEY,
-        house TEXT,
-        men INTEGER,
-        ships INTEGER,
-        money INTEGER,
-        gift_last_time INTEGER,
-        loan_last_time INTEGER,
-        pirate_last_time INTEGER,
-        pray_last_time INTEGER,
-        raid_last_time INTEGER,
-        smuggle_last_time INTEGER,
-        spy_last_time INTEGER,
-        subvert_last_time INTEGER,
-        thief_last_time INTEGER,
-        train_last_time INTEGER,
-        work_last_time INTEGER
-      );
-    `).run();
-    // Ensure that the "id" row is always unique and indexed.
-    sql.prepare(`
-      CREATE UNIQUE INDEX idx_player_data_id ON player_data (user);
-    `).run();
-    sql.pragma("synchronous = 1");
-    sql.pragma("journal_mode = wal");
-  }
-
-  const loan_table = sql.prepare(`
-    SELECT count(*) FROM sqlite_master
-    WHERE type='table' AND name = 'loans';
-  `).get();
-  if (!loan_table['count(*)']) {
-    // If the table isn't there, create it and setup the database correctly.
-    sql.prepare(`
-      CREATE TABLE loans (
-        loan_id INTEGER PRIMARY KEY,
-        user TEXT,
-        amount_due INTEGER,
-        time_due INTEGER,
-        FOREIGN KEY(user) REFERENCES player_data(user)
-      );
-    `).run();
-    // Ensure that the "id" row is always unique and indexed.
-    sql.prepare(`
-      CREATE INDEX idx_loan_user_id ON loans (user);
-    `).run();
-    sql.pragma("synchronous = 1");
-    sql.pragma("journal_mode = wal");
-  }
-
-  client.getPlayer = sql.prepare("SELECT * FROM player_data WHERE user = ?");
-  client.setPlayer = sql.prepare(`
-    INSERT OR REPLACE INTO player_data (
-      user, house, men, ships, money, pray_last_time,
-      gift_last_time, loan_last_time, pirate_last_time,
-      pray_last_time, raid_last_time, smuggle_last_time,
-      spy_last_time, subvert_last_time, thief_last_time,
-      train_last_time, work_last_time)
-    VALUES (
-      @user, @house, @men, @ships, @money, @pray_last_time,
-      @gift_last_time, @loan_last_time, @pirate_last_time,
-      @pray_last_time, @raid_last_time, @smuggle_last_time,
-      @spy_last_time, @subvert_last_time, @thief_last_time,
-      @train_last_time, @work_last_time);
-  `);
-  client.getPlayerLoans = sql.prepare("SELECT * FROM loans WHERE user = ?");
-  client.addPlayerLoan = sql.prepare(`
-    INSERT INTO loans (
-      user, amount_due, time_due)
-    VALUES (
-      @user, @amount_due, @time_due);
-  `);
-  client.updatePlayerLoan = sql.prepare(`
-    UPDATE loans SET amount_due = @amount_due WHERE loan_id = @loan_id;
-  `);
-  client.removePlayerLoan = sql.prepare(`
-    DELETE FROM loans WHERE loan_id = @loan_id;
-  `);
-  client.defaultPlayerData = {
-    "user": '',
-    "house": '',
-    "men": 20,
-    "ships": 2,
-    "money": 2000,
-    "gift_last_time": 0,
-    "loan_last_time": 0,
-    "pirate_last_time": 0,
-    "pray_last_time": 0,
-    "raid_last_time": 0,
-    "smuggle_last_time": 0,
-    "spy_last_time": 0,
-    "subvert_last_time": 0,
-    "thief_last_time": 0,
-    "train_last_time": 0,
-    "work_last_time": 0
-  };
+  // Setup database commands for various actions.
+  client.getPlayer = db.get_player;
+  client.setPlayer = db.set_player;
+  client.getPlayerLoans = db.get_loan;
+  client.addPlayerLoan = db.add_loan;
+  client.updatePlayerLoan = db.update_loan;
+  client.removePlayerLoan = db.remove_loan;
+  client.defaultPlayerData = db.default_player;
 
   console.log(`Logged in as ${client.user.tag}!`);
 });
