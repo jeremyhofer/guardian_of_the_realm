c798a1537c428c8bf20a08e8b683d0eb031f8b94
Jeremy Hofer <fuego0607@gmail.com>
2019-12-12 18:26:26 -0500

    ADD cooldown command to show times until each command may be used again

--- commands/general.js
+++ commands/general.js
@@ -1,5 +1,6 @@
 const assets = require('../assets.js');
 const db = require('../database.js');
+const utils = require('../utils.js');
 
 // Show help text. optional specific command
 const help = () => null;
@@ -10,8 +11,8 @@ const help = () => null;
  */
 const bal = ({player_data}) => {
   let reply = `Your account: ${player_data.money} :moneybag: ` +
-  `${player_data.men} ${assets.emojis.MenAtArms} ${player_data.ships} ` +
-  `${assets.emojis.Warship}`;
+    `${player_data.men} ${assets.emojis.MenAtArms} ${player_data.ships} ` +
+    `${assets.emojis.Warship}`;
 
   reply += "\n\nSiege Contributions:\n";
 
@@ -32,6 +33,41 @@ const bal = ({player_data}) => {
   return {reply};
 };
 
+const cooldown = ({player_data, command_dispatch}) => {
+  const now = Date.now();
+
+  const cooldown_map = {
+    'pirate_last_time': 'pirate',
+    'pray_last_time': 'pray',
+    'raid_last_time': 'raid',
+    'smuggle_last_time': 'smuggle',
+    'spy_last_time': 'spy',
+    'subvert_last_time': 'subvert',
+    'thief_last_time': 'thief',
+    'train_last_time': 'train',
+    'work_last_time': 'work'
+
+  };
+
+  let reply = "";
+
+  for(const key in cooldown_map) {
+    if(key in cooldown_map) {
+      const command_cooldown =
+        command_dispatch[cooldown_map[key]].cooldown.time;
+      let time_left = player_data[key] - now + command_cooldown;
+      time_left = time_left < 0
+        ? 0
+        : time_left;
+
+      const time_until_string = utils.get_time_until_string(time_left);
+      reply += `${cooldown_map[key]} ${time_until_string}\n`;
+    }
+  }
+
+  return {reply};
+};
+
 module.exports = {
   "dispatch": {
     "help": {
@@ -43,6 +79,15 @@ module.exports = {
     "bal": {
       "function": bal,
       "args": ["player_data"],
+      "command_args": [[]],
+      "usage": []
+    },
+    "cooldown": {
+      "function": cooldown,
+      "args": [
+        "player_data",
+        "command_dispatch"
+      ],
       "command_args": [[]],
       "usage": []
     }
