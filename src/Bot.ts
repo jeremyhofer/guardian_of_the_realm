2a1f643d1b47884cc4f8e2788c67a43e05aa6728
Jeremy Hofer <fuego0607@gmail.com>
2022-11-20 19:20:09 -0500

    add authentication

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,14 @@
 import { Client } from 'discord.js';
+import auth from './auth.json';
 
 console.log('Bot is starting...');
 
 const client = new Client({
   intents: []
+});
+client.login(auth.token).catch((error) => {
+  console.error('Error logging into discord API');
+  console.error(error);
 });
 
 console.log(client);
