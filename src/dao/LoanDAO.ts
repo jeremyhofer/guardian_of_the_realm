9f66ea6efa3b08e268f04e88fe58871823c4413e
Jeremy Hofer <fuego0607@gmail.com>
2022-11-20 23:55:53 -0500

    add loandao

---
+++ src/dao/LoanDAO.ts
@@ -0,0 +1,27 @@
+import { DeleteResult, LessThanOrEqual, Repository } from 'typeorm';
+import { PlayerData } from '../entity/PlayerData';
+import { Loan } from '../entity/Loan';
+
+export class LoanDAO {
+  constructor(private readonly _repository: Repository<Loan>) {}
+
+  async getLoansForUser(user: PlayerData): Promise<Loan[]> {
+    return await this._repository.findBy({
+      user
+    });
+  }
+
+  async getDueLoans(time: number): Promise<Loan[]> {
+    return await this._repository.findBy({
+      time_due: LessThanOrEqual(time)
+    });
+  }
+
+  async saveLoan(loan: Loan): Promise<Loan> {
+    return await this._repository.save(loan);
+  }
+
+  async removeLoan(loanId: number): Promise<DeleteResult> {
+    return await this._repository.delete({ loan_id: loanId });
+  }
+}
